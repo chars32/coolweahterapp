459fa7931f50d07636313f2f769308c2415b96e5
charsDev <chars24@gmail.com>
2020-11-09 14:23:49 -0600

    -- Netlify -- Checking again

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       {process.env.NODE_ENV === "development"
         ? process.env.REACT_APP_DEV_MODE
-        : process.env.REACT_APP_PRO_MODE}
+        : process.env.OPENWEAHTER_ACCES_KEY}
       {/* <Home /> */}
     </div>
   );
