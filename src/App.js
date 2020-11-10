b99a2db8f952ebaa8ff0c6247dc5740bc8abf223
charsDev <chars24@gmail.com>
2020-11-09 19:27:11 -0600

    NO MORE NETLIFY FUNCTIONS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 //Components
-// import Home from "./components/Home";
+import Home from "./components/Home";
 
 import "./App.css";
 
 function App() {
-  console.log(process.env);
   return (
     <div className="App">
-      {process.env.NODE_ENV === "development"
-        ? process.env.REACT_APP_DEV_MODE
-        : process.env.OPENWEAHTER_ACCES_KEY}
-      {/* <Home /> */}
+      <Home />
     </div>
   );
 }
